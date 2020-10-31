0a961c11355da9a449b592d0f6d5369efc75596c
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2020-10-31 23:41:21 +0200

    finished model

--- kalman.py
+++ kalman.py
@@ -7,16 +7,19 @@ class KalmanFilter(object):
     def __init__(self, F = None, B = None, H = None, Q = None, R = None, P = None, x0 = None):
 
         self.n = F.shape[1]     #numebr of variables in the system
-        self.m = H.shape[1]
+        self.m = H.shape[1]     #number of measurments
 
-        self.F = F
-        self.H = H
+        self.F = F  #prediction matrix
+        self.H = H  #sensor matrix
         self.B = 0 if B is None else B  #the control matrix
+        
         #np.eye creates an empty matrix with a diagonal of 1s
-        self.Q = np.eye(self.n) if Q is None else Q
-        self.R = np.eye(self.n) if R is None else R
-        self.P = np.eye(self.n) if P is None else P
-        self.x = np.zeros((self.n, 1)) if x0 is None else x0
+        self.P = np.eye(self.n) if P is None else P     #covariance matrix
+        self.Q = np.eye(self.n) if Q is None else Q     #noise
+        
+        self.R = np.eye(self.n) if R is None else R     #uncertainity covariance matrix
+        
+        self.x = np.zeros((self.n, 1)) if x0 is None else x0    #x0 is the inital state/values
 
 
 
@@ -26,11 +29,12 @@ class KalmanFilter(object):
         self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
         return self.x
 
+    # z is the sensor readings
     def update(self, z):
-        y = z - np.dot(self.H, self.x)
+        y = z - np.dot(self.H, self.x)  #observations - the old state
         S = self.R + np.dot(self.H, np.dot(self.P, self.H.T))
-        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
-        self.x = self.x + np.dot(K, y)
-        I = np.eye(self.n)
+        K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))  #kalman gain
+        self.x = self.x + np.dot(K, y) #corrected X
+        I = np.eye(self.n) #1 
         self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), 
-        	(I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
+        	(I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)    #corrected P
